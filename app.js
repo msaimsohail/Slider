19df288027b28ef3f3585204f1f5d311322fbbef
msaimsohail <116447889+msaimsohail@users.noreply.github.com>
2022-12-05 00:49:08 -0800

    hello world

---
+++ app.js
@@ -0,0 +1,18 @@
+let slidder = document.querySelector(".slide")
+var imge = ['im1.jpg','im2.jpg','im3.jpg']
+var i = 0
+
+function previou() {
+    if (i <= 0) i = imge.length;
+    i--;
+    return getImg();
+    }
+function next(){
+    if (i >= imge.length -1) i = -1;
+    i++;
+    return getImg();
+}
+function getImg() {
+    console.log(slidder)
+    slidder.setAttribute('src','images/' + imge[i])
+}
